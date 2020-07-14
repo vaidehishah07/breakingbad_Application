a574f0f4cb8da7b42ae59f0980273b7db9bd44e3
vaidehishah07 <shahvaidehi12@gmail.com>
2020-07-13 23:58:59 -0700

    Passing Query

--- src/App.js
+++ src/App.js
@@ -9,24 +9,25 @@ import "./App.css";
 const App = () => {
   const [items, setItems] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [query, setQuery] = useState("");
 
   useEffect(() => {
     const fetchItems = async () => {
       const result = await axios(
-        `https://www.breakingbadapi.com/api/characters`
+        `https://www.breakingbadapi.com/api/characters?name=${query}`
       );
-      console.log(result.data);
+      // console.log(result.data);
       setItems(result.data);
       setIsLoading(false);
     };
 
     fetchItems();
-  }, []);
+  }, [query]);
 
   return (
     <div className="container">
       <Header></Header>
-      <Search></Search>
+      <Search getQuery={(q) => setQuery(q)}> </Search>
       <CharacterGrid isLoading={isLoading} items={items}></CharacterGrid>
     </div>
   );
