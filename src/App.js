36835631c82fbf7ad5cb3beb58144e67065d9f1c
vaidehishah07 <shahvaidehi12@gmail.com>
2020-07-13 21:41:55 -0700

    till CharGrid

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Header from "./components/ui/Header";
+import CharacterGrid from "./components/characters/CharacterGrid";
+
+import axios from "axios";
 import "./App.css";
 
 const App = () => {
+  const [items, setItems] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchItems = async () => {
+      const result = await axios(
+        `https://www.breakingbadapi.com/api/characters`
+      );
+      console.log(result.data);
+      setItems(result.data);
+      setIsLoading(false);
+    };
+
+    fetchItems();
+  }, []);
+
   return (
     <div className="container">
       <Header></Header>
+      <CharacterGrid isLoading={isLoading} items={items}></CharacterGrid>
     </div>
   );
 };
---
+++ src/components/characters/CharacterGrid.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const CharacterGrid = ({ isLoading, items }) => {
+  return isLoading ? (
+    <h1>Loading...</h1>
+  ) : (
+    <section className="cards">
+      {items.map((item) => (
+        <h1>{item.name}</h1>
+      ))}
+    </section>
+  );
+};
+
+export default CharacterGrid;
